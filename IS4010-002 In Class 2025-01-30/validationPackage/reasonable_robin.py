4f2d5f3dc582ec36d6ac68812e20ea9be7278fde
Ryan Jacob <jacobry@mail.uc.edu>
2025-01-30 09:03:34 -0500

    Added underscored validator

--- IS4010-002 In Class 2025-01-30/validationPackage/reasonable_robin.py
+++ IS4010-002 In Class 2025-01-30/validationPackage/reasonable_robin.py
@@ -1,3 +1,22 @@
 # reasonable_robin.py
+#Ryan Jacob
 
 # Enforce at least one underscore
+
+
+def validate_password(pwd):
+    """
+    #this function validates if there is an underscore in the password
+    # @param pwd: input string to test
+    # return bool: true if pwd contains underscore, false if it doesnt
+    """
+
+    if "_" in pwd:
+        return True
+    else:
+        return False
+        
+
+    
+
+    
