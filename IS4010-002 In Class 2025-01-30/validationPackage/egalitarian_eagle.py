73c4adaf509370827e565a4104d6a5c9d9a486a9
Vanshika Rana <ranava@mail.uc.edu>
2025-01-30 08:43:59 -0500

    enforce minimum length requirement.

--- IS4010-002 In Class 2025-01-30/validationPackage/egalitarian_eagle.py
+++ IS4010-002 In Class 2025-01-30/validationPackage/egalitarian_eagle.py
@@ -1,3 +1,14 @@
+# Vanshika Rana
+#ranava@mail.uc.edu
+
+
 # egalitarian_eagle.py
 
 # Enforce Minimum Length Requirement
+
+min_length = 5
+
+def check_password_length(password, min_length=5):
+    if len(password) < min_length:
+        return False
+    return True
